0305a20929e7298719fc295df4234968bdc0b23b
CallerOfSouls <111716372+CallerOfSouls@users.noreply.github.com>
2022-10-17 08:46:52 -0400

    Add files via upload

---
+++ NuxtStarter/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./storyblok/**/*.{vue,js,ts}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
